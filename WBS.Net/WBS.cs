c9686c35070ecb98ac7f09f5a4afbb284a1c563f
Stumpy <stumpyfr@gmail.com>
2012-07-24 15:59:18 +0200

    Added GetMeasures from "lastUpdate" and offet and limit options

--- WBS.Net/WBS.cs
+++ WBS.Net/WBS.cs
@@ -82,61 +82,105 @@ namespace WBS.Net
 
                     this.UpdateProfil();
                 });
-
-            //// make a request for a protected resource
-            //string responseText0 = session.Request(accessToken).Get().ForUrl(string.Format("http://wbsapi.withings.net/user?action=update&userid={0}", userId)).ToString();
-            //string responseText = session.Request(accessToken).Get().ForUrl(string.Format("http://wbsapi.withings.net/user?action=getbyuserid&userid={0}", userId)).ToString();
-            //string responseText2 = session.Request(accessToken).Get().ForUrl(string.Format("http://wbsapi.withings.net/measure?action=getmeas&userid={0}", userId)).ToString();
         }
 
         public async Task<List<MeasureGroup>> GetMeasures(DateTime startDate, DateTime endDate)
         {
+            return await Get(string.Format("http://wbsapi.withings.net/measure?action=getmeas&userid={0}&startdate={1}&enddate={2}", userId, ToUnixTime(startDate), ToUnixTime(endDate)));
+        }
+
+        public async Task<List<MeasureGroup>> GetMeasures(DateTime startDate, DateTime endDate, int offset = -1, int limit = -1)
+        {
+            var str = string.Format("http://wbsapi.withings.net/measure?action=getmeas&userid={0}&startdate={1}&enddate={2}", userId, ToUnixTime(startDate), ToUnixTime(endDate));
+            if (offset >= 0)
+                str += string.Format("&offset={0}", offset);
+            if (limit >= 0)
+                str += string.Format("&limit={0}", offset);
+
+            return await Get(str);
+        }
+
+        public async Task<List<MeasureGroup>> GetMeasures(DateTime lastUpdate)
+        {
+            return await Get(string.Format("http://wbsapi.withings.net/measure?action=getmeas&userid={0}&lastupdate={1}", userId, ToUnixTime(lastUpdate)));
+        }
+
+        public async Task<List<MeasureGroup>> GetMeasures(DateTime lastUpdate, int offset = -1, int limit = -1)
+        {
+            var str = string.Format("http://wbsapi.withings.net/measure?action=getmeas&userid={0}&lastupdate={1}", userId, ToUnixTime(lastUpdate));
+            if (offset >= 0)
+                str += string.Format("&offset={0}", offset);
+            if (limit >= 0)
+                str += string.Format("&limit={0}", offset);
+
+            return await Get(str);
+        }
+
+        private async Task<List<MeasureGroup>> Get(string request)
+        {
             return await Task.Run<List<MeasureGroup>>(() =>
             {
-                var accessToken = new AccessToken()
-                {
-                    ConsumerKey = this.consumerKey,
-                    Token = this.token,
-                    TokenSecret = this.tokenSecret,
-                };
+                var accessToken = CreateAccessToken();
 
-                string responseText = session.Request(accessToken).Get().ForUrl(string.Format("http://wbsapi.withings.net/measure?action=getmeas&userid={0}&startdate={1}&enddate={2}", userId, ToUnixTime(startDate), ToUnixTime(endDate))).ToString();
-                DataContractJsonSerializer sr = new DataContractJsonSerializer(typeof(InternalMeasure));
-                Byte[] bytes = Encoding.Unicode.GetBytes(responseText);
-                InternalMeasure mesu = null;
-                using (MemoryStream stream = new MemoryStream(bytes))
+                try
                 {
-                    mesu = sr.ReadObject(stream) as InternalMeasure;
+                    string responseText = session.Request(accessToken).Get().ForUrl(request).ToString();
+                    return TransformMeasures(responseText);
                 }
-                if (mesu != null)
+                catch (WebException ex)
                 {
-                    List<MeasureGroup> ret = new List<MeasureGroup>();
-                    foreach (var m in mesu.body.measuregrps)
+                    throw;
+                }
+            });
+        }
+
+        private AccessToken CreateAccessToken()
+        {
+            var accessToken = new AccessToken()
+            {
+                ConsumerKey = this.consumerKey,
+                Token = this.token,
+                TokenSecret = this.tokenSecret,
+            };
+            return accessToken;
+        }
+
+        private static List<MeasureGroup> TransformMeasures(string responseText)
+        {
+            DataContractJsonSerializer sr = new DataContractJsonSerializer(typeof(InternalMeasure));
+            Byte[] bytes = Encoding.Unicode.GetBytes(responseText);
+            InternalMeasure mesu = null;
+            using (MemoryStream stream = new MemoryStream(bytes))
+            {
+                mesu = sr.ReadObject(stream) as InternalMeasure;
+            }
+            if (mesu != null)
+            {
+                List<MeasureGroup> ret = new List<MeasureGroup>();
+                foreach (var m in mesu.body.measuregrps)
+                {
+                    var me = new MeasureGroup();
+                    me.Attribution = (AttributionType)m.attrib;
+                    me.Category = (CategoryType)m.category;
+                    me.Date = FromUnixTime(m.date);
+                    me.Id = m.grpid;
+
+                    me.Measures = new List<Measure>();
+                    foreach (var internalMe in m.measures)
                     {
-                        var me = new MeasureGroup();
-                        me.Attribution = (AttributionType)m.attrib;
-                        me.Category = (CategoryType)m.category;
-                        me.Date = FromUnixTime(m.date);
-                        me.Id = m.grpid;
-
-                        me.Measures = new List<Measure>();
-                        foreach (var internalMe in m.measures)
-                        {
-                            var newMe = new Measure();
-                            newMe.MeasureType = (MeasureType)internalMe.type;
-                            newMe.Value = internalMe.value * (Math.Pow(10, internalMe.unit));
-                            me.Measures.Add(newMe);
-                        }
-
-                        ret.Add(me);
+                        var newMe = new Measure();
+                        newMe.MeasureType = (MeasureType)internalMe.type;
+                        newMe.Value = internalMe.value * (Math.Pow(10, internalMe.unit));
+                        me.Measures.Add(newMe);
                     }
 
-                    return ret;
+                    ret.Add(me);
                 }
-                else
-                    throw new Exception();
-            });
 
+                return ret;
+            }
+            else
+                throw new Exception();
         }
 
         private async void UpdateProfil()
@@ -168,7 +212,6 @@ namespace WBS.Net
                 }
             });
         }
-
 
         public static DateTime FromUnixTime(long unixTime)
         {
--- Wbs.Sample/MainWindow.xaml.cs
+++ Wbs.Sample/MainWindow.xaml.cs
@@ -20,11 +20,11 @@ namespace Wbs.Sample
     /// </summary>
     public partial class MainWindow : Window
     {
-        private const string consumerKey = "";
-        private const string consumerSecret = "";
-        private const string token = "";
-        private const string tokenSecret = "";
-        private const int userId = -1;
+        private const string consumerKey = "8232ce439c8236c240156d7955423239cc44621ed3668c03fd17b1f30";
+        private const string consumerSecret = "2f7401d0c5707fb3cd951bc07c7c4d9c1657d8ce284fc47e669d633b0552479";
+        private const string token = "c99115c176cdb3154f919c24d3cc7bd694c36301521e9c7ddd8e421c329454";
+        private const string tokenSecret = "e6c9e10997c7888c5e6c485b6e6079b88ffeedf8d95cbff7194c0055c46";
+        private const int userId = 815991;
 
         WBS.Net.WBS wbs = new WBS.Net.WBS(
             consumerKey,
@@ -32,7 +32,7 @@ namespace Wbs.Sample
             token, // optionnal
             tokenSecret, // optionnal
             userId); // optionnal 
-         // if you dont see these optionnal parameter, you will need to manage the oauth validation (see below)
+        // if you dont see these optionnal parameter, you will need to manage the oauth validation (see below)
 
         public MainWindow()
         {
@@ -47,9 +47,9 @@ namespace Wbs.Sample
             var list = await wbs.GetMeasures(DateTime.Now.AddYears(-1), DateTime.Now);
 
             // if we dont have token, tokenSecret and userId, we need to get a oauth token
-                //var url = await wbs.Connect();
-                //this.WebBrowser.LoadCompleted += WebBrowser_LoadCompleted;
-                //this.WebBrowser.Source = new Uri(url);
+            //var url = await wbs.Connect();
+            //this.WebBrowser.LoadCompleted += WebBrowser_LoadCompleted;
+            //this.WebBrowser.Source = new Uri(url);
         }
 
         void WebBrowser_LoadCompleted(object sender, NavigationEventArgs e)
